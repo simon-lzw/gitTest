468fbbce5e14bbb9d26b10dc501c4ef38bcbd9a2
Administrator <Administrator@jab41351>
2016-12-27 12:09:54 +0800

    Signed-off-by: Administrator <Administrator@jab41351>

---
+++ luzhiwei/src/luzhiwei/test.java
@@ -0,0 +1,14 @@
+package luzhiwei;
+
+public class test {
+	int a=0, b=0 ;
+	int i=a+b;
+fjlskjglkjaslgkjlfaksjlglsjlfkjds
+fklshglksfja
+sgkfsdfgljaksf
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
